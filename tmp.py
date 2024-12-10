a1f8aa44853392d0c9e98302fe3be6211d4d3773
李怡霈 <yipei0917@gmail.com>
2024-12-10 17:53:48 +0800

    fix bug

--- tmp.py
+++ tmp.py
@@ -1,9 +1,9 @@
 import requests
-def check_task_completion(task_id):
+def check_task_completion(task_id, target_area, target_product):
     # When robot arrives at destination
     try:
         response = requests.post(f'http://127.0.0.1:5000/api/robot/arrived',
-                                    json={'task_id': task_id})
+                                    json={'task_id': task_id, 'target_area': target_area, 'target_product': target_product})
         return response.json()
     except Exception as e:
         print(f"Error notifying server: {e}")
@@ -15,5 +15,5 @@ def restock_prdouct(targer_area, target_product):
     except Exception as e:
         print(f"Error notifying server: {e}")
 
-# print(check_task_completion(1))
+# print(check_task_completion(1, 'Lamp Area', 'Lamp_1'))
 print(restock_prdouct('Lamp Area', 'Lamp_1'))
